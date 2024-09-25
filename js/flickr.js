79c33e6e99e13253a5f08211590c9ddb566f2883
tubi55 <tubi55@nate.com>
2024-09-25 14:50:23 +0900

    갤러리 스타일링

--- js/flickr.js
+++ js/flickr.js
@@ -4,16 +4,10 @@ const baseURL = `https://www.flickr.com/services/rest/?api_key=${api_key}&method
 const myID = "197119297@N02";
 const method_mine = "flickr.people.getPhotos";
 let url = `${baseURL}${method_mine}&user_id=${myID}&nojsoncallback=1&format=json`;
-//기본요청URL에 쿼리값 제대로 넣어서 fetch처리했을때
-//콘솔문에 not valid json 오류뜨는 경우
-//원인 : json데이터가 callback함수안에 들어가 있는 경우
-//해결방법 : format=json&nojsoncallback=1 (callback안쪽의 json데이터를 직접 가져옴)
-// 2시 5분까지 위의 방법으로 데이터가 잘 불러와지는지 테스트
 
 fetch(url)
   .then((data) => data.json())
   .then((json) => {
-    console.log(json.photos.photo);
     const picArr = json.photos.photo;
     let tags = "";
 
@@ -21,7 +15,7 @@ fetch(url)
       tags += `
         <li>
           <figure class='pic'>
-            <img src="https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg" alt={pic.title}
+            <img src="https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg" alt=${pic.title} />
           </figure>
           <h2>${pic.title}</h2>
 
