a2213418017f1909855b2b84f66e59aec0805556
tubi55 <tubi55@nate.com>
2024-09-26 11:43:35 +0900

    검색 갤러리 기능 추가

--- js/flickr.js
+++ js/flickr.js
@@ -1,52 +1,63 @@
-const [btnMine, btnPopular] = document.querySelectorAll("nav button");
 let dataType = "";
+const [btnMine, btnPopular] = document.querySelectorAll("nav button");
+//searchBox안쪽에 있는 두번째 요소인 input, 세번째 요소인 btnSearch를 비구조할당으로 변수할당
+const [_, inputSearch, btnSearch] = document.querySelector(".searchBox").children;
 
-//fetch함수 호출시 인수값을 객체 형태로 전달
-//이유 : search, user타입 갤러리는 타입외에도 유저명, 검색어등의 추가 정보값을 제공해야 되기 때문
 fetchFlickr({ type: "mine" });
-
 btnMine.addEventListener("click", () => fetchFlickr({ type: "mine" }));
 btnPopular.addEventListener("click", () => fetchFlickr({ type: "interest" }));
+//검색 버튼 클릭시
+btnSearch.addEventListener("click", () => {
+	//인풋요소의 value값 (검색어)을 tags에 담아 fetchFlickr함수 호출
+	fetchFlickr({ type: "search", tags: inputSearch.value });
+	//호출시 input요소의 검색어는 지워줌
+	inputSearch.value = "";
+});
 
-document.body.addEventListener("click", (e) => {
-  if (e.target.className === "thumb") createModal(e);
-  if (e.target.className === "btnClose") removeModal();
-  if (e.target.className === "userID")
-    fetchFlickr({ type: "user", userID: e.target.innerText }); //user타입 갤러리에는 useID라는 추가 프로퍼티로 유저아이디값을 전달
+document.body.addEventListener("click", e => {
+	if (e.target.className === "thumb") createModal(e);
+	if (e.target.className === "btnClose") removeModal();
+	if (e.target.className === "userID") fetchFlickr({ type: "user", userID: e.target.innerText });
 });
 
 function fetchFlickr(opt) {
-  //타입이 opt라는 파라미터 객체안쪽에 들어가 있기 때문에 opt.type
-  if (opt.type === dataType) return;
-  dataType = opt.type;
+	if (opt.type === dataType) return;
+	dataType = opt.type;
 
-  const api_key = "21e294ad0ec03a32d7355980457d9e11";
-  const baseURL = `https://www.flickr.com/services/rest/?api_key=${api_key}&method=`;
-  const myID = "197119297@N02";
-  const method_mine = "flickr.people.getPhotos";
-  const method_interest = "flickr.interestingness.getList";
-  let url_mine = `${baseURL}${method_mine}&user_id=${myID}&nojsoncallback=1&format=json`;
-  let url_user = `${baseURL}${method_mine}&user_id=${opt.userID}&nojsoncallback=1&format=json`;
-  let url_interest = `${baseURL}${method_interest}&nojsoncallback=1&format=json`;
-  let result_url = "";
-  if (opt.type === "mine") result_url = url_mine;
-  else if (opt.type === "interest") result_url = url_interest;
-  else if (opt.type === "user") result_url = url_user;
+	const api_key = "21e294ad0ec03a32d7355980457d9e11";
+	const baseURL = `https://www.flickr.com/services/rest/?api_key=${api_key}&method=`;
+	const myID = "197119297@N02";
+	const method_mine = "flickr.people.getPhotos";
+	const method_interest = "flickr.interestingness.getList";
+	//검색전용 api 메서드 추가
+	const method_search = "flickr.photos.search";
+	let url_mine = `${baseURL}${method_mine}&user_id=${myID}&nojsoncallback=1&format=json`;
+	let url_user = `${baseURL}${method_mine}&user_id=${opt.userID}&nojsoncallback=1&format=json`;
+	let url_interest = `${baseURL}${method_interest}&nojsoncallback=1&format=json`;
+	//opt로 전달된 tags 프로퍼티의 값을 tags라는 쿼리값 연동
+	let url_search = `${baseURL}${method_search}&tags=${opt.tags}&nojsoncallback=1&format=json`;
 
-  fetch(result_url)
-    .then((data) => data.json())
-    .then((json) => {
-      const picArr = json.photos.photo;
-      createList(picArr);
-    });
+	let result_url = "";
+	if (opt.type === "mine") result_url = url_mine;
+	if (opt.type === "interest") result_url = url_interest;
+	if (opt.type === "user") result_url = url_user;
+	//전달된 opt.type값이 search이면 url_search 요청을 fetch함수에 전달
+	if (opt.type === "search") result_url = url_search;
+
+	fetch(result_url)
+		.then(data => data.json())
+		.then(json => {
+			const picArr = json.photos.photo;
+			createList(picArr);
+		});
 }
 
 function createList(dataArr) {
-  const list = document.querySelector(".list");
-  let tags = "";
+	const list = document.querySelector(".list");
+	let tags = "";
 
-  dataArr.forEach((pic) => {
-    tags += `
+	dataArr.forEach(pic => {
+		tags += `
         <li>
           <figure class='pic'>
             <img class='thumb' src="https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_z.jpg" alt="https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg" />
@@ -58,40 +69,33 @@ function createList(dataArr) {
           </div>
         </li>
       `;
-  });
+	});
 
-  list.innerHTML = tags;
+	list.innerHTML = tags;
 
-  setDefImg();
+	setDefImg();
 }
 
 function setDefImg() {
-  const profilePic = document.querySelectorAll(".profile img");
-  console.log(profilePic);
+	const profilePic = document.querySelectorAll(".profile img");
+	console.log(profilePic);
 
-  profilePic.forEach(
-    (imgEl) =>
-      (imgEl.onerror = () =>
-        imgEl.setAttribute(
-          "src",
-          "https://www.flickr.com/images/buddyicon.gif"
-        ))
-  );
+	profilePic.forEach(imgEl => (imgEl.onerror = () => imgEl.setAttribute("src", "https://www.flickr.com/images/buddyicon.gif")));
 }
 
 function createModal(e) {
-  const imgSrc = e.target.getAttribute("alt");
-  const modal = document.createElement("aside");
-  modal.classList.add("modal");
-  modal.innerHTML = `
+	const imgSrc = e.target.getAttribute("alt");
+	const modal = document.createElement("aside");
+	modal.classList.add("modal");
+	modal.innerHTML = `
       <div class='con'>
         <img src=${imgSrc} />
       </div>
       <button class='btnClose'>CLOSE</button>
     `;
-  document.body.append(modal);
+	document.body.append(modal);
 }
 
 function removeModal() {
-  document.querySelector(".modal").remove();
+	document.querySelector(".modal").remove();
 }
